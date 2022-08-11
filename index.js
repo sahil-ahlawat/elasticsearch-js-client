a41b55f9f12af793a47335008a87c60ee1f046f7
CORP\sahlawat <sahlawat@trueblue.com>
2022-08-12 02:01:42 +0530

    elastic search connection with nodejs

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const { Client } = require('@elastic/elasticsearch')
+var fs = require('fs');
 const client = new Client({
     node: 'https://localhost:9200',
   auth: {
     username: 'elastic',
     password: 'gE5KOzIBNwv=4My9R_6B'
+  },
+  tls: {
+    // ca: fs.readFileSync('./http_ca.crt'),
+    rejectUnauthorized: false
   }
 })
+
+async function test(){
+  try{
+    let result = await client.info();
+  console.log(result);
+  }
+  catch(e){
+    console.log("error");
+    console.log(e);
+  }
+  
+} 
+test();
